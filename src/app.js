3518d18277805208a873b614a130e5fa719d6b81
MastiderMast <65887954+MastiderMast@users.noreply.github.com>
2022-09-12 21:20:58 +0200

    update some

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 80;
+const port = process.env.port || 8080;
 const fs = require('fs');
 require('dotenv').config();
 
@@ -26,6 +26,9 @@ app.get("/:id", async function(req, res) {
 
 app.post("/create", async function(req, res) {
     var url = req.body.url;
+
+    checkIfUrlisValid(url);
+
     var id = addURL(url);
     var response = {
         id: id,
@@ -56,6 +59,13 @@ function addURL(url) {
         }
     })
     return id;
+}
+
+async function checkIfUrlisValid(url) {
+    //check if url is valid with regex
+    var regex = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    if(regex.test(url)) return console.log("valid");
+    else return console.log("invalid");
 }
 
 async function checkValidURLS() {
